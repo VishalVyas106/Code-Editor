68f97d699e086e77067117ce12407f8619999ac3
VishalVyas106 <vyasvishal106.work@gmail.com>
2024-11-24 16:08:14 +0530

    code editor Home page and logo and selcte lag button4

--- src/Parts/CodeLabTitle.jsx
+++ src/Parts/CodeLabTitle.jsx
@@ -3,22 +3,17 @@ import { Code2 } from 'lucide-react';
 
 const CodeLabTitle = () => {
   return (
-    <div className="flex items-center justify-center space-x-2 py-2">
-      <a href="https://code-lab-seven.vercel.app/" target="_blank">
-
-      <Code2 className="w-6 h-6 text-blue-400" />
-      <h1 className="text-xl font-bold tracking-tight bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent">
-        CodeLab
-      </h1>
+    <div className="flex items-center justify-center py-2">
+      <a href="https://code-lab-seven.vercel.app/" target="_blank" className="flex items-center space-x-2">
+        <span className="flex items-center space-x-2">
+          <Code2 className="w-6 h-6 text-blue-400" />
+          <h1 className="text-xl font-bold tracking-tight bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent">
+            CodeLab
+          </h1>
+        </span>
       </a>
     </div>
   );
 };
+
 export default CodeLabTitle;
-
-
-
-
-
-
-
