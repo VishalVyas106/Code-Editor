bf9a093f2a303c4c5298f8717d46cb22bff896a0
Vyas Vishal <vyasvishal106.work@gmail.com>
2024-11-24 15:33:24 +0530

    Update CodeLabTitle.jsx

--- src/Parts/CodeLabTitle.jsx
+++ src/Parts/CodeLabTitle.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Code2 } from 'lucide-react';
+import { NavLink } from 'react-router-dom';
 
 const CodeLabTitle = () => {
   return (
     <div className="flex items-center justify-center space-x-2 py-2">
-        <Code2 className="w-6 h-6 text-blue-400" />
-        <h1 className="text-xl font-bold tracking-tight bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent">
-      <a href="https://code-lab-seven.vercel.app/" target="_blank" rel="noopener noreferrer">
-          CodeLab
+      <a href="https://code-lab-seven.vercel.app/" target="_blank">
+
+      <Code2 className="w-6 h-6 text-blue-400" />
+      <h1 className="text-xl font-bold tracking-tight bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent">
+        CodeLab
+      </h1>
       </a>
-        </h1>
     </div>
   );
 };
